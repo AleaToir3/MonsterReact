9238ead5c25537359fd6fa60c9a84bf2723772c2
AleaToir3 <aleatoir3@gmail.com>
2024-05-20 01:05:17 +0100

    monster and  monster input search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+ import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-je fait un test         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ 
+
+    constructor() {
+      super();
+      this.state = {
+        monsters:[],
+        searchFild : ""
+       };
+   }
+ 
+
+
+  componentDidMount() {
+  
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res=>res.json())
+    .then(apiMonsters => this.setState(
+      ()=>{
+        return {monsters : apiMonsters}
+      },
+      ()=>{
+       }
+    ))
+  }
+  
+  render() {
+     const filterMonster = this.state.monsters.filter( monster =>monster.name.toLowerCase().includes(this.state.searchFild))
+    console.log(this.state.searchFild)
+       return (
+      <div className="App">
+        <header className="App-header">
+        <div>
+                <input placeholder="Entre votre recherche" onChange={(e)=>{  
+                  this.setState({searchFild : e.target.value.toLowerCase()})
+
+                 }}></input> 
+                 {  
+                  filterMonster.map((monster =>{
+                  return <p key={monster.name}>{monster.name}</p>
+                }))}
+                
+
+        </div>
+        </header>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;

d6dceb0ea5dfc4cf72dddedb40670be995c6fe94
AleaToir3 <aleatoir3@gmail.com>
2024-05-20 01:14:13 +0100

    performence on code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
- import "./App.css";
+import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
- 
 
-    constructor() {
-      super();
-      this.state = {
-        monsters:[],
-        searchFild : ""
-       };
-   }
- 
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchFild: ""
+    };
+  }
+
 
 
   componentDidMount() {
-  
+
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res=>res.json())
-    .then(apiMonsters => this.setState(
-      ()=>{
-        return {monsters : apiMonsters}
-      },
-      ()=>{
-       }
-    ))
+      .then(res => res.json())
+      .then(apiMonsters => this.setState(
+        () => {
+          return { monsters: apiMonsters }
+        },
+        () => {
+        }
+      ))
   }
-  
+
+  onSearchChange = (e) => {
+    this.setState({ searchFild: e.target.value.toLowerCase() })
+  }
+
   render() {
-     const filterMonster = this.state.monsters.filter( monster =>monster.name.toLowerCase().includes(this.state.searchFild))
-    console.log(this.state.searchFild)
-       return (
+
+    const filterMonster = this.state.monsters.filter(monster => monster.name.toLowerCase().includes(this.state.searchFild))
+
+    return (
       <div className="App">
         <header className="App-header">
-        <div>
-                <input placeholder="Entre votre recherche" onChange={(e)=>{  
-                  this.setState({searchFild : e.target.value.toLowerCase()})
+          <div>
+            <input placeholder="Entre votre recherche" onChange={this.onSearchChange}></input>
+            {
+              filterMonster.map((monster => {
+                return <p key={monster.name}>{monster.name}</p>
+              }))}
 
-                 }}></input> 
-                 {  
-                  filterMonster.map((monster =>{
-                  return <p key={monster.name}>{monster.name}</p>
-                }))}
-                
 
-        </div>
+          </div>
         </header>
       </div>
     );
   }
 }
- 
+
 export default App;
